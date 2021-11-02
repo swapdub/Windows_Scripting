01129a02dde11980a9280f341bc28fdee9166cd0
Swapnil Dubey <32624172+swapdub@users.noreply.github.com>
2021-11-02 16:32:46 -0400

    League of Legends Login
    Working copy to automate App Login

---
+++ league_login_script.bat
@@ -0,0 +1,33 @@
+@REM Author: swapdub
+@REM Source: https://stackoverflow.com/questions/17038282/press-keyboard-keys-using-a-batch-file
+
+@REM Other Instructions:
+@REM - Create shortcut for your file
+@REM - Paste shortcut in:
+@REM     C:\Users\*YourUserName*\AppData\Roaming\Microsoft\Windows\Start Menu\Programs
+@REM Now it would be available in Windows Start Menu and you can Pin to Start Menu
+
+@if (@CodeSection == @Batch) @then
+@echo off
+
+rem Use %SendKeys% to send keys to the keyboard buffer
+set SendKeys=CScript //nologo //E:JScript "%~F0"
+
+start "" /B "C:\Your\Path\to\Riot Games\Riot Client\RiotClientServices.exe" --launch-product=league_of_legends --launch-patchline=live
+echo "League of Legends is loading. PLEASE WAIT..."
+timeout /t 8 /nobreak
+
+
+%SendKeys% "username"
+@REM timeout /t 1 /nobreak
+%SendKeys% "{TAB}"
+@REM timeout /t 1 /nobreak
+%SendKeys% "password"
+@REM timeout /t 1 /nobreak
+%SendKeys% "{ENTER}"
+
+@end
+// JScript section
+
+var WshShell = WScript.CreateObject("WScript.Shell");
+WshShell.SendKeys(WScript.Arguments(0));
